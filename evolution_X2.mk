11ac5f12a1c4501293d9cbca1f8823fb4ea3c1b4
Abungo Thokchom <abungothokchom@gmail.com>
2021-09-23 21:56:25 +0530

    evolution_x2 edited

--- evolution_X2.mk
+++ evolution_X2.mk
@@ -7,18 +7,18 @@
 $(call inherit-product, device/realme/X2/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_GAPPS_ARCH := arm64
-WITH_GAPPS := true
-TARGET_INCLUDE_STOCK_ARCORE := true
+WITH_GAPPS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_WIFI_EXT := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_X2
+PRODUCT_NAME := evolution_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
