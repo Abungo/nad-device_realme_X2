7faf042855a130e8b2f56c07c6d30b4cfd4e78dc
Abungo Thokchom <abungothokchom@gmail.com>
2021-09-23 21:53:43 +0530

    AndroidProducts.mk edited

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X2.mk
+    $(LOCAL_DIR)/evolution_X2.mk
 
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X2-eng \
-    aosp_X2-user \
-    aosp_X2-userdebug
+    evolution_X2-eng \
+    evolution_X2-user \
+    evolution_X2-userdebug
